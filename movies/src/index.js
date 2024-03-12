dd9fdf61b4636f1d4f668b700e97d4fca353c49d
Diarmuid <20071587@mail.wit.ie>
2024-03-12 10:37:48 +0000

    Added Upcoming movies page

--- movies/src/index.js
+++ movies/src/index.js
@@ -5,7 +5,8 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
+import Upcoming from './pages/upcoming';
 
 
 const App = () => {
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
+        <Route path="/movies/upcoming" element={<Upcoming />} />
         <Route path="/" element={<HomePage />} />
         <Route path="*" element={ <Navigate to="/" /> } />
       </Routes>
