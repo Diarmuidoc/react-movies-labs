671f95caf129ed075e386fa30c37af039a0b1960
Diarmuid <20071587@mail.wit.ie>
2024-03-19 10:43:25 +0000

    New button for upcoming movie, no longer favorite from upcoming

---
+++ movies/src/components/cardIcons/addToWatchlist.js
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
+import IconButton from "@mui/material/IconButton";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+
+const AddToWatchlistIcon = ({ movie }) => {
+  const context = useContext(MoviesContext);
+
+  return (
+    <IconButton aria-label="add to watchlist">
+      <PlaylistAddIcon color="primary" fontSize="large" />
+    </IconButton>
+  );
+};
+
+export default AddToWatchlistIcon;
