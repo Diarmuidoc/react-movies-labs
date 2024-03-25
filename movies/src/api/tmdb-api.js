61a0a275fc78342109f9d130b66205a951d61265
Diarmuid <20071587@mail.wit.ie>
2024-03-25 16:59:01 +0000

    Added Top rated page, plus some minor changes to other pages to start assignment 1

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -86,3 +86,19 @@ export const getMovie = (args) => {
        throw error
     });
   };
+
+
+  //Assignment 1, get top rated
+  export const getTopRated = () => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+       throw error
+    });
+  };
--- movies/src/index.js
+++ movies/src/index.js
@@ -6,11 +6,14 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader';
-import Upcoming from './pages/upcoming';
+import Upcoming from './pages/upcomingPage';
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
+
+//a1
+import TopRated from './pages/topRatedPage';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -30,13 +33,13 @@ const App = () => {
         <MoviesContextProvider>
         <Routes>
           <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
-          {/* <Route path="/movies/upcoming" element={<UpcomingMoviesPage />} /> */}
           <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
           <Route path="/movies/:id" element={<MoviePage />} />
           <Route path="/movies/upcoming" element={<Upcoming />} />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={ <Navigate to="/" /> } />
           <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
+          <Route path="/movies/topRated" element={<TopRated />} />
         </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
